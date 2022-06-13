ec28e0c4758e15102445df02ba6f043c6a52ebba
deshaxii <mustafa.fathi7@gmail.com>
2022-06-13 13:30:27 +0200

    add storage section, time now 1:30 pm

--- src/pages/documents.jsx
+++ src/pages/documents.jsx
@@ -17,7 +17,7 @@ function Documents() {
                 </button>
               </div>
               <div className="my-files-body">
-                <div className="file-box">
+                <div className="file-box documents">
                   <div className="file-box__icon">
                     <IoMdDocument />
                   </div>
@@ -34,7 +34,7 @@ function Documents() {
                     </div>
                   </div>
                 </div>
-                <div className="file-box">
+                <div className="file-box google_drive">
                   <div className="file-box__icon">
                     <IoMdDocument />
                   </div>
@@ -43,7 +43,7 @@ function Documents() {
                   </div>
                   <div className="file-box__bottom">
                     <div className="file-box__bar">
-                      <span style={{ width: "60%" }}></span>
+                      <span style={{ width: "30%" }}></span>
                     </div>
                     <div className="file-box__info">
                       <span className="files-count">1,328 Files</span>
@@ -51,7 +51,7 @@ function Documents() {
                     </div>
                   </div>
                 </div>
-                <div className="file-box">
+                <div className="file-box one_drive">
                   <div className="file-box__icon">
                     <IoMdDocument />
                   </div>
@@ -60,7 +60,7 @@ function Documents() {
                   </div>
                   <div className="file-box__bottom">
                     <div className="file-box__bar">
-                      <span style={{ width: "60%" }}></span>
+                      <span style={{ width: "75%" }}></span>
                     </div>
                     <div className="file-box__info">
                       <span className="files-count">1,328 Files</span>
@@ -68,7 +68,7 @@ function Documents() {
                     </div>
                   </div>
                 </div>
-                <div className="file-box">
+                <div className="file-box dropbox">
                   <div className="file-box__icon">
                     <IoMdDocument />
                   </div>
@@ -77,7 +77,7 @@ function Documents() {
                   </div>
                   <div className="file-box__bottom">
                     <div className="file-box__bar">
-                      <span style={{ width: "60%" }}></span>
+                      <span style={{ width: "50%" }}></span>
                     </div>
                     <div className="file-box__info">
                       <span className="files-count">1,328 Files</span>
@@ -89,7 +89,64 @@ function Documents() {
             </section>
           </div>
           <div className="documents-page-layout__right">
-            <section className="storage-details">storage</section>
+            <section className="storage-details">
+              <div className="storage-details-header">
+                <h5>Storage Details</h5>
+              </div>
+              <div className="storage-body">
+                <div className="storage-body__chart"></div>
+                <div className="storage-body__info">
+                  <div className="storage-body__info-item documents">
+                    <div className="sbit">
+                      <div className="sbit-icon">
+                        <IoMdDocument />
+                      </div>
+                      <div className="sbit-info">
+                        <h6>Documents Files</h6>
+                        <span>1,328 files</span>
+                      </div>
+                    </div>
+                    <span className="size">1.3GB</span>
+                  </div>
+                  <div className="storage-body__info-item media">
+                    <div className="sbit">
+                      <div className="sbit-icon">
+                        <IoMdDocument />
+                      </div>
+                      <div className="sbit-info">
+                        <h6>Media Files</h6>
+                        <span>1,328 files</span>
+                      </div>
+                    </div>
+                    <span className="size">15.1GB</span>
+                  </div>
+                  <div className="storage-body__info-item other">
+                    <div className="sbit">
+                      <div className="sbit-icon">
+                        <IoMdDocument />
+                      </div>
+                      <div className="sbit-info">
+                        <h6>Other Files</h6>
+                        <span>1,328 files</span>
+                      </div>
+                    </div>
+                    <span className="size">12.7GB</span>
+                  </div>
+                  <div className="storage-body__info-item unknown">
+                    <div className="sbit">
+                      <div className="sbit-icon">
+                        <IoMdDocument />
+                      </div>
+                      <div className="sbit-info">
+                        <h6>Unknown</h6>
+                        <span>428 files</span>
+                      </div>
+                    </div>
+                    <span className="size">1.3GB</span>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
